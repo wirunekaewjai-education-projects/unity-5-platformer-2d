3ee29de3e6339dbbe171a064ac37f3cdf4227994
Wirune Kaewjai <wirune.kae@hotmail.com>
2015-09-20 22:57:53 +0700

    Create Menu Scene (Continue Function is In-progress)

---
+++ Assets/Scripts/Menu Scene/Menu.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public int newGameLevelIndex = 1;
+
+    public Button continueButton;
+    public GameObject panel1, panel2;
+
+    void Awake()
+    {
+        if (PlayerPrefs.GetInt("Saved", 0) == 0)
+        {
+            continueButton.interactable = false;
+        }
+    }
+
+    public void OnNewGameClick()
+    {
+        if(PlayerPrefs.GetInt("Saved", 0) == 0)
+        {
+            Application.LoadLevel(newGameLevelIndex);
+        }
+        else
+        {
+            panel1.SetActive(false);
+            panel2.SetActive(true);
+        }
+    }
+
+    public void OnContinueClick()
+    {
+        int levelIndex = PlayerPrefs.GetInt("LevelIndex", newGameLevelIndex);
+
+    }
+
+    public void OnQuitClick()
+    {
+        Application.Quit();
+    }
+
+    public void OnNoButton()
+    {
+        panel2.SetActive(false);
+        panel1.SetActive(true);
+    }
+
+    public void OnYesButton()
+    {
+        Application.LoadLevel(newGameLevelIndex);
+    }
+
+
+}
